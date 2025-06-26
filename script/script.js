a55dcdb6650f9849bad63be3314631ba3666c6df
andrii <Andrewponomare@gmail.com>
2025-06-26 19:08:46 +0300

    add discount to currency

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
 let usdCurr = 41.7;
 let eurCurr = 49.5;
+let discount = 1.1;
 
 function convert(curr, amount) {
-    console.log(curr * amount)
+    let number = curr * amount;
+    return number;
 }
 
-convert(usdCurr, 100)
-convert(eurCurr, 100)
+
+
+function promotion(number, discount) {
+    console.log(number * discount);
+}
+
+let res = convert(usdCurr, 100);
+promotion(res, discount);
